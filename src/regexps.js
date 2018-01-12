5e9d2b7ff626add2ab972dc8d755f9279f59944d
erwanledoux <lawanledoux@gmail.com>
2018-01-12 18:50:43 +0100

    added getFilterRegExp

--- src/regexps.js
+++ src/regexps.js
@@ -10,3 +10,6 @@ constantTestNames.forEach(name =>
   constantTestsByName[`${camelCase(name)}Test`] = new RegExp(`${constantsByName[name]}(.*)`))
 
 export const newTest = new RegExp(`${constantsByName.NEW}(.*)`)
+
+export const getFilterRegExp = (name, maxLength = 10) =>
+  new RegExp(`[${name}]{${Math.min(name && name.length, maxLength)}}`, 'i')
